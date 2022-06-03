0c67930dc5883d5e3dc963c10f7a265c9a61ce03
Atm-Gregr <greg.rastklan@atm-consulting.fr>
2022-06-03 12:10:21 +0200

    family

--- core/modules/modAttachments.class.php
+++ core/modules/modAttachments.class.php
@@ -55,14 +55,14 @@ class modAttachments extends DolibarrModules
 
 		// Family can be 'crm','financial','hr','projects','products','ecm','technic','other'
 		// It is used to group modules in module setup page
-		$this->family = "ATM Consulting - Autre";
+		$this->family = 'ATM Consulting';
 		// Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
 		$this->name = preg_replace('/^mod/i','',get_class($this));
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Description of module Attachments";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
 
-		$this->version = '1.2.1';
+		$this->version = '1.2.2';
 		// Url to the file with your last numberversion of this module
 		require_once __DIR__ . '/../../class/techatm.class.php';
 		$this->url_last_version = \attachments\TechATM::getLastModuleVersionUrl($this);
